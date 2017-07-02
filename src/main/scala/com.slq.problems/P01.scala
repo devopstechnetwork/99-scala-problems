693b09ea639c915352d16773699424026d5ebb4d
Slawek Plamowski <slawek.plamowski@gmail.com>
2017-07-02 13:09:55 +0200

    Move to correct subpackages

--- src/main/scala/com.slq.problems/P01.scala
+++
@@ -1,9 +0,0 @@
-package com.slq.problems
-
-class P01[T] {
-  def last(list: List[T]): Option[T] = list match {
-    case Nil => None
-    case head :: Nil => Some(head)
-    case head :: tail => last(tail)
-  }
-}
